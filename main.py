4f69ed45de3b14886da06c4ce27cb3586eee7de7
Aleksandra Krzywicka <30755785+aleksandrachmurka@users.noreply.github.com>
2018-07-19 22:49:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ import continent_counter
 
 
 def main():
-    coordinates = (5, 5)
+    coordinates = [5, 5]
     list_of_tiles = []
     list_of_close_tiles = []
     
-    list_of_tiles = continent_counter.generate_random_world(coordinates, list_of_close_tiles)
+    list_of_tiles = continent_counter.generate_random_world(list_of_tiles, coordinates, list_of_close_tiles)
     sorted_list_of_tiles = continent_counter.sort_list_of_tiles_by_x_y(list_of_tiles)
     list_of_close_tiles = continent_counter.count_area(sorted_list_of_tiles, coordinates, list_of_close_tiles)
+    print(list_of_tiles)
     print(list_of_close_tiles)
     print("The area of your continent is: " + str(len(list_of_close_tiles)))
 
 
 if __name__ == '__main__':
     main()
+
