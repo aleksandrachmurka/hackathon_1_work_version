a41df59276d8503f17b886820c1fa1398b5f9a8d
Aleksandra Krzywicka <30755785+aleksandrachmurka@users.noreply.github.com>
2018-07-19 22:26:59 +0200

    Update continent_counter.py

--- continent_counter.py
+++ continent_counter.py
@@ -1,24 +1,31 @@
 import random
 import operator
 
+coordinates = (5, 5)
 list_of_tiles = []
 list_of_close_tiles = []
 
+world_size_x = int(input('Welcome! Now enter an integer. Your world should have size of: '))
+world_size_y = int(input('Since we want it to be a 2D world, please enter another integer of your choice: '))
 
-def generate_random_world():
+
+def generate_random_world(list_of_close_tiles, coordinates):
     random.seed()
-    list_of_tiles = [(random.randint(0, 11), random.randint(0, 11)) for k in range(30)]  # here we have to add some code to not repeat tiles
+    list_of_close_tiles.append(coordinates)
+    for k in range(30):
+        new_tile =(random.randint(0, world_size_x), random.randint(0, world_size_y))
+        for tile in list_of_tiles:
+            if new_tile[0] == tile [0]:
+                if new_tile[1] == tile[1]:
+                    break
+            else:
+                list_of_tiles.append(new_tile)
     return list_of_tiles
 
 
 def sort_list_of_tiles_by_x_y(list_of_tiles):
     list_of_tiles = sorted(list_of_tiles, key=operator.itemgetter(0, 1))
     return list_of_tiles
-
-
-def add_default_coordinates(coordinates):
-    list_of_close_tiles.append(coordinates)
-    return list_of_close_tiles
 
 
 def count_area(list_of_tiles, coordinates, list_of_close_tiles):
